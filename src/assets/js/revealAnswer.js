c9e49fc02c67da5d7fb83db04af41a785ba6acf2
ahisu6 <123895039+ahisu6@users.noreply.github.com>
2023-03-10 21:27:55 +0000

    Update revealAnswer.js

--- src/assets/js/revealAnswer.js
+++ src/assets/js/revealAnswer.js
@@ -14,13 +14,3 @@ if (temp1.className=="answerBlock") {
     // temp.innerHTML = 'Hide Answer';
 }
 }
-
-/*
-@@html:<div onclick="reveal()">Click this sentence to see the answers! <span class="jp">この文章をクリックすると、答えが表示されます！</span></div><div style="display: none;">@@
-
-These can be in Org-mode text/list/etc.
-Answer goes here
-Blah, blah...
-
-@@html:</div>@@
-*/
