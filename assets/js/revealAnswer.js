7702aa86a22099e47932e2676840c6e6f1dfa7c9
ahisu6 <123895039+ahisu6@users.noreply.github.com>
2023-03-10 21:31:29 +0000

    Update 3299c66b80e53347dddda232795764a90a33aa69

--- assets/js/revealAnswer.js
+++ assets/js/revealAnswer.js
@@ -1,16 +1,1 @@
-// I am grateful for the answer provided by the user "am2505": https://stackoverflow.com/questions/61302125/hiding-answers-to-questions-using-javascript
-
-function reveal($event) {
-  // console.log(event);
-  var temp = event.target;
-  var temp1 = event.target.nextElementSibling;
-  // console.log(temp);
-  // console.log(temp1);
-if (temp1.className=="answerBlock") {
-    temp1.classList.remove("answerBlock");
-    // temp.innerHTML = 'Show Answer';
-} else {
-    temp1.classList.add("answerBlock");
-    // temp.innerHTML = 'Hide Answer';
-}
-}
+function reveal(e){event.target;var a=event.target.nextElementSibling;"answerBlock"==a.className?a.classList.remove("answerBlock"):a.classList.add("answerBlock")}
