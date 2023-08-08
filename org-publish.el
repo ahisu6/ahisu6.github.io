2205ad72c658e12cf7c0f08dfee08da5fda0b60d
です <123895039+ahisu6@users.noreply.github.com>
2023-08-08 02:07:13 +0100

    Update org-publish.el

--- org-publish.el
+++ org-publish.el
@@ -19,7 +19,7 @@
       (list
        (list "ahisu6"
              :recursive t
-             :base-directory "./src2"
+             :base-directory "./copy/src"
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
              :with-author nil
